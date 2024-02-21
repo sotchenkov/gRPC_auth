8ddab438fc370c2562a53029df861d55d1d76903
Alexey <sotchenkoff@gmail.com>
2024-02-21 18:39:56 +0300

    functional testing

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # gRPC_auth
 gRPC SSO service
 
+```bash
+go mod download 
+go run ./cmd/migrator --storage-path=./storage/sso.db --migrations-path=./migrations
+go run cmd/sso/main.go --config=./config/local.yaml
+```
+
 [Protobuf contract](https://github.com/sotchenkov/protos)
 
 [Reference](https://github.com/GolangLessons/sso)
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,15 +9,16 @@ import (
 )
 
 type Config struct {
-	Env string `yaml:"env" env-default:"local"`
-	StoragePath string `yaml:"storage_path" env-required:"true"`
-	TokenTTL time.Duration `yaml:"token_ttl" env-required:"true"`
-	GRPC GRPCConfig `yaml:"grpc"`
+	Env            string     `yaml:"env" env-default:"local"`
+	StoragePath    string     `yaml:"storage_path" env-required:"true"`
+	GRPC           GRPCConfig `yaml:"grpc"`
+	MigrationsPath string
+	TokenTTL       time.Duration `yaml:"token_ttl" env-default:"1h"`
 }
 
 type GRPCConfig struct {
-	Port int `yaml:"port" env-required:"true"`
-	Timeout time.Duration `yaml:"timeout" env-required:"true"`
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
 }
 
 func MustLoad() *Config {
@@ -26,24 +27,30 @@ func MustLoad() *Config {
 		panic("config path is empty")
 	}
 
+	return MustLoadPath(configPath)
+}
+
+func MustLoadPath(configPath string) *Config {
+	// check if file exists
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		panic("config path is not exist: " + configPath)
+		panic("config file does not exist: " + configPath)
 	}
 
 	var cfg Config
 
-	if err:=cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		panic("failed to read config: " + err.Error())
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
 	}
 
 	return &cfg
 }
 
-// Priority: flag > env > default
-// Default value is empty string
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
 func fetchConfigPath() string {
 	var res string
-	
+
 	flag.StringVar(&res, "config", "", "path to config file")
 	flag.Parse()
 
